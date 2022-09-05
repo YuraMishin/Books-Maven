71f08978547853257a9b017b16f74eac5681295f
Mishin Yura <mishin.inbox@gmail.com>
2022-09-05 08:17:32 +0300

    Add entity Book. Closes #4

---
+++ src/main/java/com/mishinyura/booksmaven/models/Book.java
@@ -0,0 +1,38 @@
+package com.mishinyura.booksmaven.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
