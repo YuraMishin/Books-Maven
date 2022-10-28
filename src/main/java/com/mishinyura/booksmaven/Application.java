d683805d87d5509d2d5ca8f369668ed2226a0ba0
Mishin Yura <mishin.inbox@gmail.com>
2022-10-28 08:38:40 +0300

    Refactor application.propeties to *.yaml. Closes #79

--- src/main/java/com/mishinyura/booksmaven/Application.java
+++ src/main/java/com/mishinyura/booksmaven/Application.java
@@ -4,10 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("@PostConstruct example ok");
     }
 }
